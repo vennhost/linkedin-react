aff57ea9fd74db2d212189c70b689709bab1cc8e
vennhost <samogconsult@gmail.com>
2019-12-03 16:50:55 +0100

    corrected

--- src/Components/ProfileHeading.jsx
+++ src/Components/ProfileHeading.jsx
@@ -5,15 +5,16 @@ class ProfileHeading extends React.Component {
     state = {  }
     render() { 
         return ( 
-            <div className="my-5">
+            <div className="">
                 <img className="profile-photo" src={this.props.profile.image} alt="my pic"/>
             <div>
-                <img className="profile-photo" src={this.props.profile.image}  width="30%" alt="my pic"/>
+                {/* <img className="profile-photo" src={this.props.profile.image}  width="30%" alt="my pic"/> */}
                 <h3>{this.props.profile.name} {this.props.profile.surname}</h3>
                 <h4>{this.props.profile.title}</h4>
                 <h5>{this.props.profile.area}</h5>
             </div>
             </div>
+           
          );
     }
 }
