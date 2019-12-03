22fa6c7195d2c5fbcdf1a85406723dc6ae68b35f
vennhost <samogconsult@gmail.com>
2019-12-03 10:37:37 +0100

    nav

--- src/Components/ProfileHeading.jsx
+++ src/Components/ProfileHeading.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import '../index.css'
 
 class ProfileHeading extends React.Component {
     state = {  }
     render() { 
         return ( 
             <div className="my-5">
-                <img src={this.props.profile.image} alt="my pic"/>
+                <img className="profile-photo" src={this.props.profile.image} alt="my pic"/>
                 <h3>{this.props.profile.name} {this.props.profile.surname}</h3>
             </div>
          );
