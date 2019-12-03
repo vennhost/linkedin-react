75234abe1d061e3490df9b72f5cc332555ebb51b
zabed <zabedalam777@gmail.com>
2019-12-03 16:00:29 +0100

    modal chages

--- src/Components/ProfileModal.jsx
+++ src/Components/ProfileModal.jsx
@@ -1,25 +1,61 @@
 import React from "react"
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Row,Col,Form,Label,Input,FormGroup } from 'reactstrap';
 
 
 class ProfileModal extends React.Component {
     state = { 
+      name: "",
+        surname: "",
+        title: "",
+        bio: "",
+        area: "",
+        image: "",
       modal:false
      }
 
-     modalClose(){
+     modalClose=()=>{
        this.setState({
          modal:false
        })
      }
     render() { 
+      
+
         return ( 
             <div>
       
       <Modal isOpen={this.props.open}>
-        <ModalHeader>{this.props.profile.name}</ModalHeader>
+        <ModalHeader><h3>Edit intro</h3></ModalHeader>
         <ModalBody>
-            {this.props.profile.bio}
+        
+                             <Form>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="firstName">First Name</Label>
+            <Input type="text"  id="firstName"  />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="lastname">Last Name</Label>
+            <Input type="text" id="lastname"  />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Label for="headline">Head Line</Label>
+        <Input type="text"  id="headline" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="currentPosition">Current Position</Label>
+        <Input type="text"  id="currentPosition" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="area">Area</Label>
+        <Input type="text"  id="area" />
+      </FormGroup>
+      </Form> 
         </ModalBody>
         <ModalFooter>
           <Button color="primary" >Save</Button>{' '}
