a0c72bfd22f2a94d782afacb597891b001f6a78c
vennhost <samogconsult@gmail.com>
2019-12-03 10:48:16 +0100

    changes from merge

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-<<<<<<< HEAD
 import {Container} from 'reactstrap'
 import NavBar from './NavBar';
 import Profile from './Profile';
 import '../index.css'
-=======
 import {} from 'reactstrap'
 import NavBar from './NavBar'
 import Profile from './Profile'
 import Banner from './Banner';
->>>>>>> 9cd4977f4a2db933b152b2869227bcf58e6c3455
 
 class MainPage extends React.Component {
     state = {  }
@@ -18,15 +15,12 @@ class MainPage extends React.Component {
         return ( 
             <>
             <NavBar />
-<<<<<<< HEAD
             
             <Container className="profile mt-5">
             <Profile></Profile>
             </Container>
-=======
             <Banner></Banner>
             {/* <Profile></Profile> */}
->>>>>>> 9cd4977f4a2db933b152b2869227bcf58e6c3455
             
 
             </>
