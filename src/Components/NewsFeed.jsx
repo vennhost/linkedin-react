35cbc91b28bc4e209957dd055cf0499278cbba09
vennhost <samogconsult@gmail.com>
2019-12-05 05:54:31 +0100

    Newsfeed

---
+++ src/Components/NewsFeed.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {} from 'reactstrap';
+
+
+class NewsFeed extends Component {
+    state = { 
+        posts: []
+     }
+    render() { 
+        return ( 
+            <>
+                <div>
+                   <ProfileHeading /> 
+                </div>
+                <div>
+                    {this.state.posts.map((post, index) => 
+                        <div className="p-3 bg-info my-2 rounded">
+                            <Toast key={index}>
+                            <ToastHeader icon="info">
+                                {post.username}
+                            </ToastHeader>
+                            <ToastBody>
+                               {post.text}
+                            </ToastBody>
+                            </Toast>
+                        </div>
+                        )}
+                </div>
+            </>
+            );
+    }
+
+    componentDidMount=async ()=>{
+        let response=await fetch("https://strive-school-testing-apis.herokuapp.com/api/posts/",{
+            headers:{
+                "Authorization":"basic dXNlcjIwOlkyY0paMzhVUE1tblBkQVc="
+            }
+        })
+        let posts = await response.json()
+        this.setState({
+            posts: posts
+        })
+    }
+}
+ 
+export default NewsFeed;
