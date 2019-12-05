f916989447aac1450fadcd4d860d3c040ca6260d
zabed <zabedalam777@gmail.com>
2019-12-05 16:40:08 +0100

    image issue

--- src/Components/FileUpload.jsx
+++ src/Components/FileUpload.jsx
@@ -7,7 +7,7 @@ class FileUpload extends React.Component {
      }
 
     fileSelectedHandler=event=>{
-// console.log(event.target.files[0])
+ console.log(event.target.files[0])
 this.setState({
     selectedFile:event.target.files[0]
 })
@@ -15,15 +15,15 @@ this.setState({
 
     fileUploadHandler=async()=>{
         const fd=new FormData();
-     let uploadPic=   fd.append("image",this.state.selectedFile,this.state.selectedFile.image)
-     await imagePost(uploadPic)
-    }
+        let uploadPic=   fd.append("image",this.state.selectedFile.image)
+        await imagePost(uploadPic)
+     }
     render() { 
         return ( 
             <div>
 <input type="file" style={{display:"none"}} onChange={this.fileSelectedHandler} ref={fileInput=>this.fileInput=fileInput}/>
 <button onClick={()=>this.fileInput.click()}>Pick image</button>
-<button onClick={this.fileUploadHandler}>Upload Image</button>
+<button onClick={()=>this.fileUploadHandler()}>Upload Image</button>
             </div>
          );
     }
--- src/Components/ImagePostApi.jsx
+++ src/Components/ImagePostApi.jsx
@@ -1,13 +1,14 @@
-const imagePost = async () => {
+const imagePost = async (img) => {
     let URL = "https://striveschool.herokuapp.com/api/profile/user20/picture/"
     try {
         let response = await fetch(URL, {
             method: "POST",
+            body:img,
             headers: {
                 "Authorization": "Basic dXNlcjIwOlkyY0paMzhVUE1tblBkQVc=" ,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify()
+                "Content-Type": "image/jeg"
+            }
+            
         })
         if (response.ok) {
             return await response.json()
